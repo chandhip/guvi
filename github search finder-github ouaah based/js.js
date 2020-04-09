792aeb60c90a45562fb1f1a445474ba9cc45b0bd
Divyath ali shajahan <38224894+Divyathali@users.noreply.github.com>
2020-04-09 19:09:48 +0530

    Add files via upload

---
+++ github search finder-github ouaah based/js.js
@@ -0,0 +1,17 @@
+document.getElementById("demo").innerHTML="hii diiivivv";
+function mouseOver1() {
+  document.getElementById("demo").style.color = "red";
+}
+
+function mouseOut2() {
+  document.getElementById("demo").style.color = "black";
+}#abc * {
+  color: currentcolor;
+}
+function mouseOver() {
+  document.getElementById("abc").style.color = "red";
+}
+
+function mouseOut() {
+  document.getElementById("abc").style.color = "black";
+}
